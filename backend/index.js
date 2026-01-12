41a490edcfc67aff7378e6f1768b45411f5299f0
Surakshith <surakshith1904@gmail.com>
2026-01-12 17:50:52 +0530

    we made when we type in one window it is displayed in window another window without changing in our window

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,12 @@ app.get('/', (req,res) => {
 io.on('connection',(socket) => {
   console.log(`User connected: ${socket.id}`);
 
+  // 1. Listen for the "send_message" event from the Frontend
+  socket.on("send_message", (data) => {
+    // 2. When we receive a message, we emit it to all connected clients
+    socket.broadcast.emit("receive_message", data);
+  });
+
   socket.on('disconnect', () => {
     console.log(`User disconnected: ${socket.id}`);
   });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,39 @@
-import { useEffect } from "react";
+import { useEffect , useState} from "react";
 import io from "socket.io-client";
 
 const socket = io("http://localhost:5000"); // Connect to the backend server
 
 function App() {
+  // 1. "message" is what you are typing right now.
+  const [message, setMessage] = useState("");
 
+  const [messageReceived, setMessageReceived] = useState("");
+  // 2. "messageReceived" is what the other person sent you
+  
+  const sendMessage = () => {
+    socket.emit("send_message", { message }); // Emit the message to the server
+  }
   useEffect(() => {
-    //this code will when the app starts
-    console.log("Connecting to socket server...");
-  }, []);
+    // This keeps the ear open for messages coming FROM the Backend
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
 
   return (
-    <div style={{padding: "20px"}} >
-      <h1>Socket.io Frontend</h1>
-      <p>Check the console for connection status.</p>
+    <div style={{ padding: "50px" }}>
+      <h1>Simul-Project</h1>
+      
+      <input 
+        placeholder="Type something..." 
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message </button>
+
+      <h3>Message from other user:</h3>
+      <p style={{color: "red", fontSize: "24px"}}>{messageReceived}</p>
     </div>
   );
 }
