2c2f3bf8b964b65794baf26c59fb3176b8cc5f34
Surakshith <surakshith1904@gmail.com>
2026-01-31 16:13:26 +0530

    made a notification pop up when partner joins the room

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast"; // <--- IMPORT THIS
 import Home from "./components/Home";
 import EditorPage from "./components/EditorPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/editor/:roomId" element={<EditorPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      {/* Configure Toast Styles for Dark Mode */}
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          style: {
+            background: '#333',
+            color: '#fff',
+          },
+        }}
+      />
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/editor/:roomId" element={<EditorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
